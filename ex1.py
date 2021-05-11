746e0be5b3c82073ec48e62edcf3279bcfd209e0
elias qubty <elias-m-q-92@hotmail.com>
2021-05-11 17:05:58 +0300

    OrderDict Commit

--- ex1.py
+++ ex1.py
@@ -1,3 +1,22 @@
+"""
+collections.OrderedDict
+Exercise
+You are the manager of a supermarket.
+You have a list of N items together with their prices that consumers bought on a particular day. Your task is to print each item_name and net_price in order of its first occurrence.
+
+item_name = Name of the item.
+net_price = Quantity of the item sold multiplied by the price of each item.
+
+Input Format
+The first line contains the number of items, .
+The next  lines contain the item's name and price, separated by a space.
+
+Constraints
+N <= 100
+Output Format
+Print the item_name and net_price in order of its first occurrence.
+
+"""
 from collections import OrderedDict
 print("Enter the number of items :" )
 n, d = int(input()), OrderedDict()
