ce19efebabdefcba04a919a6f99066cd8184c4b2
elias qubty <elias-m-q-92@hotmail.com>
2021-05-11 17:08:56 +0300

    Counter

--- ex2.py
+++ ex2.py
@@ -1,3 +1,23 @@
+"""
+collections.Counter()
+
+Exercise
+
+Rahue is a shoe shop owner. His shop has  X number of shoes.
+He has a list containing the size of each shoe he has in his shop.
+There are  N number of customers who are willing to pay xi amount of money only if they get the shoe of their desired size.
+Your task is to compute how much money  Rahue earned.
+
+Input Format
+The first line contains X , the number of shoes.
+The second line contains a space separated list of all the shoe sizes in the shop.
+The third line contains N, the number of customers.
+The next N lines contain the space separated values of the shoe size desired by the customer and xi  the price of the shoe. 
+
+Output Format
+Print the amount of money earned by Rahue.
+
+"""
 from collections import Counter
 print("Enter the number of shoes :" ) 
 number_of_shoes=input()
